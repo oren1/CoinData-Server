68ce590e17ba0e70345e69cdaa2bb1bb494a2099
Oren Shalev <orenico4@gmail.com>
2020-10-28 14:06:44 +0200

    changed bitfinex instructions tex

--- Exchanges/ExchangesInfo.js
+++ Exchanges/ExchangesInfo.js
@@ -1,7 +1,7 @@
 const PermissionKeys = {
     API_KEY: 'apiKey',
     API_SECRET: 'apiSecret',
-    USER_ID: 'userId',
+    USER_ID: 'apiUserId',
 }
 
 const exchanges = {
@@ -10,7 +10,7 @@ const exchanges = {
             name: "Bitfinex",
             keyName: "bitfinex",
             logoUrl: "https://www.cryptocompare.com/media/37071996/bitfinex.png",
-            instructions: ["string1", "string2","string3","string4"],
+            instructions: ["1. string1\n", "2. string2\n","3. string3\n","4. string4\n"],
             permissionKeys: [ 
                 {
                     key: PermissionKeys.API_KEY,
