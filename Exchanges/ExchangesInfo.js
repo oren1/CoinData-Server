f6457bdb0a93347c6a0a472a467d9d6eae949fc0
Oren Shalev <orenico4@gmail.com>
2020-10-28 14:34:39 +0200

    changed bitfinex instructions

--- Exchanges/ExchangesInfo.js
+++ Exchanges/ExchangesInfo.js
@@ -10,7 +10,12 @@ const exchanges = {
             name: "Bitfinex",
             keyName: "bitfinex",
             logoUrl: "https://www.cryptocompare.com/media/37071996/bitfinex.png",
-            instructions: ["1. string1\n", "2. string2\n","3. string3\n","4. string4\n"],
+            instructions: ["1. Log in to your Bitfinex account and go to Account -> API\n", 
+            "2. Select Create new key tab\n",
+            "3. Select all read boxes, it should be selected by default.\n",
+            "4. Label your API KEY\n",
+            "5. Scan the QR Code or enter the api key and api secret manualy"],
+
             permissionKeys: [ 
                 {
                     key: PermissionKeys.API_KEY,
